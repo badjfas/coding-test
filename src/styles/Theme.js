9333b1c64a41b36a7a534f63fa19b5e42b55706a
badjfas <bjwkor@naver.com>
2020-12-09 20:39:00 +0900

    add theme property

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,9 +1,13 @@
 export default {
-    primaryBlueColor: "#1565C0;",
-    blueColor: "2196F3", //font,
-    fontGray: "939fa5",
-    borderGray: "e5e5e5",
+    primaryBlueColor: "#1565C0",
+    blueColor: "#2196F3", //font,
+    fontGray: "#939fa5",
+    fontDarkGray: "#323D45",
+    borderGray: "#e5e5e5",
+    lightBlueColor: "#BBDEFB",
     bold: 700,
     normal: 400,
-    buttonFontWeight: 500
+    buttonFontWeight: 500,
+    breakpointMobile: "320px",
+    breakpointTablet: "768px"
 };
